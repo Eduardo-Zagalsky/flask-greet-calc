2a510cb4d7fe4f972171ec9886ac453638043f2f
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-08-19 00:14:25 -0500

    added calc logic and tested it

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,38 @@
-# Put your app in here.
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/add")
+def add():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a + b)
+
+@app.route("/sub")
+def sub():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a - b)
+
+@app.route("/mult")
+def mult():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a * b)
+
+@app.route("/div")
+def div():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(a / b)
+
+@app.route("/math/<operation>")
+def all_in_one(operation):
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    ops = {
+        "add": a + b,
+        "sub": a - b,
+        "mult": a * b,
+        "div": a / b
+    }
+    return str(ops[operation])
